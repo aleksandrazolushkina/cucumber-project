acf40e17794beee4f6dc52d4ec5bfacfa4ef873d
Aleksandra Zolushkina <aleksandra.zolushkina@jetbrains.com>
2020-06-30 16:08:46 +0200

    initial commit

---
+++ test/java/com/examples/cucumber/MyStepdefs.java
@@ -0,0 +1,24 @@
+package com.examples.cucumber;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class MyStepdefs {
+    @Given("I have <opening balance> beer cans")
+    public void iHaveOpeningBalanceBeerCans() {
+    }
+
+    @And("I have drunk <processed> beer cans")
+    public void iHaveDrunkProcessedBeerCans() {
+    }
+
+    @When("I go to my fridge")
+    public void iGoToMyFridge() {
+    }
+
+    @Then("I should have <in stock> beer cans")
+    public void iShouldHaveInStockBeerCans() {
+    }
+}
---
+++ test/java/com/examples/cucumber/RunTestNew.java
@@ -0,0 +1,12 @@
+package com.examples.cucumber;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = {"classpath:features/BeerCans.feature"},
+        glue = {"com.examples.cucumber"})
+public class RunTestNew {
+}
